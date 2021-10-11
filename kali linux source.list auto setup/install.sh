a7503be1959caed0e80b33bdafaaa77f7280218a
Dirty_devil <69082359+codingwithdevil@users.noreply.github.com>
2021-10-11 08:11:45 +0530

    Update install.sh

--- kali linux source.list auto setup/install.sh
+++ kali linux source.list auto setup/install.sh
@@ -3,8 +3,11 @@ python3 install.py
 python3 insta.py
 echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" | sudo tee /etc/apt/sources.list
 echo "deb-src http://http.kali.org/kali kali-rolling main contrib non-free" | sudo tee -a /etc/apt/sources.list
-apt update
-apt upgrade
+apt-get update
+apt-get upgrade
 apt dist-upgrade
 apt autoremove
+tasksel
+apt-get update
+apt-get upgrade
 python3 installing.py
